f2cc8d7af50e605ffe4870e949338d70576ea9b9
Yihui Hu <yh3203@columbia.edu>
2019-12-04 17:32:46 +0000

    yhu 2nd commit: command line file in sightings

---
+++ project/sightings/management/commands/import_squirrel_data.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+import pandas as pd
+import sys
+import csv, sqlite3
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('file_url', nargs='+', type=str, help='URL of the data file')
+
+    def handle(self, *args, **options):
+        #print(options['file_url'])
+        conn= sqlite3.connect("db.sqlite3")
+        df = pd.read_csv(options['file_url'][0],index_col=0)
+        df.to_sql('file_url', conn, if_exists='append', index=False)
+
+        #df = pd.DataFrame([(0,1),(2,3)],columns=['col1','col2'])
+        print('ok')    
+
+    
