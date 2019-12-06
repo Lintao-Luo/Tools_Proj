9131065ed362a04dc89afbc7e12279090e07871d
Lintao-Luo <56945042+Lintao-Luo@users.noreply.github.com>
2019-12-06 18:20:13 -0500

    Add map

--- project/Squirrel/urls.py
+++ project/Squirrel/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('',views.index, name='index'),
     path('sightings/', views.sightings, name='sightings'),
     path('sightings/add', views.add, name='add'),
+    path('map/', views.get_map, name='map'),
 ]
