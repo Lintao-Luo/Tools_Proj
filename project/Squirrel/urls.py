6d3310f4a72510d9effdfc0db38f0a89e6487f12
Lintao Luo <ll3243@columbia.edu>
2019-12-07 22:30:53 +0000

    Final_0

--- project/Squirrel/urls.py
+++ project/Squirrel/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('sightings/add', views.add, name='add'),
     path('sightings/<str:squirrel_id>/', views.update, name='update'),
     path('map/', views.get_map, name='map'),
+    path('sightings/stats', views.get_stats, name='stats'),
 ]
--- project/Squirrel/views.py
+++ project/Squirrel/views.py
@@ -54,3 +54,14 @@ def update(request, squirrel_id):
     }
 
     return render(request, 'Squirrel/add.html', context)
+
+def get_stats(request):
+    total_num = Squirrel.objects.all().count()
+    s1_adult_rate = Squirrel.objects.filter(age='Adult').count()/total_num
+    s2_ground_plane_rate = Squirrel.objects.filter(location='Ground Plane').count()/total_num
+    s3_running_rate = Squirrel.objects.filter(running=True).count()/total_num
+    s4_chasing_rate = Squirrel.objects.filter(chasing=True).count()/total_num
+    s5_eating_rate = Squirrel.objects.filter(eating=True).count()/total_num
+    stats_dict = {'s1':s1_adult_rate, 's2':s2_ground_plane_rate, 's3':s3_running_rate, 's4':s4_chasing_rate, 's5':s5_eating_rate}
+    return render(request, 'Squirrel/stats.html', stats_dict)
+
