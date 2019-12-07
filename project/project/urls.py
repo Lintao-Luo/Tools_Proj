6b18a70e13067823c2325e545e879ea487c2b71d
YihuiHu-0822 <56944890+YihuiHu-0822@users.noreply.github.com>
2019-12-07 16:45:16 -0500

    master clean

--- project/project/urls.py
+++ project/project/urls.py
@@ -18,5 +18,4 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('adopt/', include('adopt.urls')), # Here we include the file 'urls' in the app adopt.
 ]
